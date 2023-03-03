163f97819cd5af64f50d131f6b31a1a6a6a7583d
Gamliel Urlanda <g.urlanda@gmail.com>
2023-03-02 16:44:53 -0800

    Initial setup

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# practice-problems-generator
+A generator of practice problems for those wanting to brush up on fundamental arithmetic.
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <header>
+      <h1>Test Question Generator</h1>
+    </header>
+  );
+}
+
+export default App;
