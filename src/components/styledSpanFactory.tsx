8841ad2364c419af642282b1551acd22e6d853f4
Gamliel Urlanda <g.urlanda@gmail.com>
2023-03-03 11:55:40 -0800

    Add documentation

--- src/components/styledSpanFactory.tsx
+++ src/components/styledSpanFactory.tsx
@@ -7,14 +7,19 @@ const StyledSpan: React.FC<{
   return <span className={classNames}>{children}</span>;
 };
 
+/**
+ * Given a string of classes, creates a reusable span that has the given class names. This is useful if you want to use many spans, all with the same style applied to them.
+ * @param classNames The class names used on the produced span.
+ * @returns A span element pre-styled with the given class names.
+ */
 function styledSpanFactory(
   classNames: string
 ): React.FC<{ children: ReactNode }> {
-  const PreColoredSpan: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const PreStyledSpan: React.FC<{ children: ReactNode }> = ({ children }) => {
     return <StyledSpan classNames={classNames}>{children}</StyledSpan>;
   };
 
-  return PreColoredSpan;
+  return PreStyledSpan;
 }
 
 export default styledSpanFactory;
